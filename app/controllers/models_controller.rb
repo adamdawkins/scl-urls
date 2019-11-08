9415592567392137d660246998cd4506bea1d506
Adam Dawkins <adam@dragondrop.uk>
2019-11-08 14:24:56 +0000

    ♻️ Remove unused code from model controller

--- app/controllers/models_controller.rb
+++ app/controllers/models_controller.rb
@@ -14,54 +14,6 @@ class ModelsController < ApplicationController
   # GET /models/1.json
   def show; end
 
-  # GET /models/new
-  def new
-    @model = Model.new
-  end
-
-  # GET /models/1/edit
-  def edit; end
-
-  # POST /models
-  # POST /models.json
-  def create
-    @model = Model.new(model_params)
-
-    respond_to do |format|
-      if @model.save
-        format.html { redirect_to @model, notice: 'Model was successfully created.' }
-        format.json { render :show, status: :created, location: @model }
-      else
-        format.html { render :new }
-        format.json { render json: @model.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /models/1
-  # PATCH/PUT /models/1.json
-  def update
-    respond_to do |format|
-      if @model.update(model_params)
-        format.html { redirect_to @model, notice: 'Model was successfully updated.' }
-        format.json { render :show, status: :ok, location: @model }
-      else
-        format.html { render :edit }
-        format.json { render json: @model.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /models/1
-  # DELETE /models/1.json
-  def destroy
-    @model.destroy
-    respond_to do |format|
-      format.html { redirect_to models_url, notice: 'Model was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
   private
 
   def set_model
